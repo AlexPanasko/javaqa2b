73678551dc16f99540e2f2d7b746f501a1caa5ed
Alex Panasko <a.panasko@bk.ru>
2023-03-19 23:27:01 +0500

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int startingBalance = 400;
+        int refill = 1100;
+
+        int bonus = 0;
+        if (refill > 1000) {
+            bonus = refill / 100;
+        }
+
+        int finalBalance = startingBalance + refill + bonus;
+        System.out.println("Баланс: " + finalBalance);
+        System.out.println("Бонус: " + bonus);
+
     }
 }
